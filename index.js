c4cbb1ccf50bb2b4bd17f8517921c3913f393db6
unknown <designwithtawhid@gmail.com>
2025-01-08 00:23:30 +0600

    show asssigmne m id

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", async (req, res) => {
   res.send("Class is Running................");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhrby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,13 @@ async function run() {
       const newAssignment = req.body;
       const result = await AssignmentCollection.insertOne(newAssignment);
       console.log(result);
+      res.send(result);
+    });
+
+    app.get("/assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await AssignmentCollection.findOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
